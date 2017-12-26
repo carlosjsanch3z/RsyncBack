12a98dcb0bff36da6af48f8dd05b34ffe1d409b3
Carlos Jesús Sánchez Ortega <carlosjsanchezortega@gmail.com>
2017-12-26 23:19:20 +0100

    Estructura Script

--- mickey.sh
+++ mickey.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
-# Comprobar conectividad
-
+# Comprobar dia de la semana
+DOW=$(date +%u)
+# Comprobar conectividad con el servidor remoto
 ping -q -w 1 -c 1 PublicIP > /dev/null
 
 if [ $? -eq 0 ]; then
-  # Hacer el insert
-  /usr/bin/psql -h 172.22.200.110 -d db_backup -U carlos.sanchez -c "INSERT backups into values'"
-  if [ $? -eq 0 ]; then
-    # Enviar por correo el aviso de que hay que ingresar el registro de la copia manualmente
+  if [[ $DOW -eq 7 ]]; then
+    # Hacer una copia completa
+    rsync full
+    # Guardar el registro de la copia en Coconut
+    /usr/bin/psql -h 172.22.200.110 -d db_backup -U carlos.sanchez -c "INSERT backups into values'200'"
+    if [ $? -ne 0 ]; then
+      # Avisar por correo, ingresar registro manualmente de esta máquina
+  else
+    # Hacer una copia incremental
+    rsync incremental
+    # Guardar el registro de la copia incremental en Coconut
+    /usr/bin/psql -h 172.22.200.110 -d db_backup -U carlos.sanchez -c "INSERT backups into values'200'"
+    if [ $? -ne 0 ]; then
+      # Avisar por correo, ingresar registro manualmente de esta máquina
+  fi
 else
   # Si no hay conexión, modificar el backup_status y añadir el fallo
-  /usr/bin/psql -h 172.22.200.110 -d db_backup -U carlos.sanchez -c
+  /usr/bin/psql -h 172.22.200.110 -d db_backup -U carlos.sanchez -c "INSERT backups into values'500'"
   if [[ $? -ne 0 ]]; then
-    # Enviar notificación de que ha fallado coconut db
+    # Enviar notificación de que ha fallado coconut db & raspberry pi
   fi
-  # Notificación no se ha podido almacenar la copia de dicha máquina en remoto
 fi
